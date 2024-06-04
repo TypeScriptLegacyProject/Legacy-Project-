09e872c1e27cb9e75539dc67aae9082e9e888655
Selimbensaid <selimbensaid11@gmail.com>
2024-06-04 22:30:20 +0100

    checked all backend functionnalites , made some changes, removed unnecessary commented functions and added two text files fasturl and statuscode

--- server/middelwares/user.js
+++ server/middelwares/user.js
@@ -2,14 +2,21 @@ const jwttoken = require("jsonwebtoken");
 
 const autoriser = (req, res, next) => {
   const token = req.headers.token;
+  if (!token) {
+    return res.status(403).send("Token not provided");
+  }
   jwttoken.verify(token, "your_generated_secret_key_here", (err, decoded) => {
-    if (err) res.send("ther is no token");
-    console.log(decoded);
+    if (err) return res.status(401).send("invalid token");
     if (decoded.role === "user") {
       next();
     } else {
-      res.send("u r not authorised");
+      res.status(403).send("u r not authorised");
     }
   });
 };
 module.exports = autoriser;
+
+
+//Changes made :
+// i added a check to return a 403 status code if the token is not given and 401 status if the token is invalid
+// and 403 if the user doesnt have the permission to access
