92193f8bf9509ff613a87a52e817d370ff3bd1e5
Selimbensaid <selimbensaid11@gmail.com>
2024-06-06 13:05:55 +0100

    changed the whole css of login/register and checked the functionnalites

--- Client/my-app/app/layout.tsx
+++ Client/my-app/app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
     <html lang="en">
    
       <body className={inter.className}>
-      <Navbar/>
+      {/* <Navbar/> */}
          {children}
-      <Footer/>
+      {/* <Footer/> */}
       </body>
     </html>
     
